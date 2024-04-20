5caac9187402c072851025bff425780b35b78cd1
FHYQ-Dong <FHYQ-Dong-Work@outlook.com>
2024-04-20 21:04:17 +0800

    modified:   src/github_action_runner.py

--- src/github_action_runner.py
+++ src/github_action_runner.py
@@ -5,6 +5,7 @@ import os, json
 if __name__ == "__main__":
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
     upload_path, result_path = r'../upload', r'../result'
+    workflow_info_file = r'../src/workflow_info.json'
     if not os.path.isdir(upload_path):
         os.makedirs(upload_path)
     if not os.path.isdir(result_path):
@@ -17,11 +18,11 @@ if __name__ == "__main__":
         'zip-file': '',
         'publish': False
     }
-    if not os.path.isfile(r'./workflow.json'):
-        with open(r'./workflow_info.json', 'w', encoding='utf-8') as f:
+    if not os.path.isfile(workflow_info_file):
+        with open(workflow_info_file, 'w', encoding='utf-8') as f:
             f.write(json.dumps(workflow_info, ensure_ascii=False, indent=4))
 
-    with open(r'./workflow_info.json', 'r', encoding='utf-8') as f:
+    with open(workflow_info_file, 'r', encoding='utf-8') as f:
         workflow_info = json.load(f)
 
     checker = check_unhandled_files.Checker(upload_path, result_path)
@@ -36,7 +37,7 @@ if __name__ == "__main__":
         workflow_info['title'] = unhandled_files[0]
         workflow_info['body'] = '\n'.join(unhandled_files)
         workflow_info['tag'] = int(workflow_info['tag']) + 1
-        workflow_info['zip-file'] = r'./result.zip'
+        workflow_info['zip-file'] = r'./result/result.zip'
         workflow_info['publish'] = True
     else:
         workflow_info['title'] = ''
@@ -44,6 +45,6 @@ if __name__ == "__main__":
         workflow_info['zip-file'] = ''
         workflow_info['publish'] = False
     
-    with open(r'./workflow_info.json', 'w', encoding='utf-8') as f:
+    with open(workflow_info_file, 'w', encoding='utf-8') as f:
         f.write(json.dumps(workflow_info, ensure_ascii=False, indent=4))
     os.system(r'zip -r ../result/result.zip ../result/*')
